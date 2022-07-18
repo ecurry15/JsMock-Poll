053a96e3ce0c86ff7aa4f6eafb4eec1cd7700531
Edward <Ecampbell3015@gmail.com>
2022-07-18 12:32:44 -0700

    finish

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ const poll = {
   question: "What is your favorite programming language?",
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   answers: new Array(4).fill(0),
+  displayResults(type = 'array') {
+    if (type =='array') {
+      console.log(this.answers);
+    } else if (type == 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+  registerNewAnswer () {
+    let userAnswer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number.)`));
+    if (typeof userAnswer === 'number' && userAnswer <= this.answers.length) {
+    this.answers[userAnswer]++ ;
+    } else {
+      alert('Invalid Number, Please submit again.')
+    }
+    this.displayResults();
+  }
 };
 
 
+button.addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.registerNewAnswer = () => {
-  let userAnswer = parseInt(window.prompt(`${poll.question}`));
-  if (userAnswer > 3) {
-    alert('Invalid Number, Please submit again.')
-  } else {
-    poll.answers[userAnswer]++ ;
-    console.log(poll.answers);
-  }
-}
-
-button.addEventListener('click', poll.registerNewAnswer);
+poll.displayResults.call({answers: [5,2,3]});
+poll.displayResults.call({answers: [1,5,3,9,6,1]}, 'string');
